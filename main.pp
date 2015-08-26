c4505227e9ea835b5c918bc5ff67d9468a750459
István Antal <istvan.m.antal@gmail.com>
2015-08-26 17:33:13 +0100

    Initial setup script

---
+++ main.pp
@@ -0,0 +1,10 @@
+include packagecloud
+
+node default {
+    package { ["curl", "openssh-server", "ca-certificates", "postfix"]: ensure => "installed" }
+
+    packagecloud::repo { "gitlab/gitlab-ce":
+        type => 'deb',
+        server_address => 'https://packages.gitlab.com/',
+    }
+}
